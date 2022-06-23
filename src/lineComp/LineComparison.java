c924538e9ca59a1ad3eb00707f382ed2078ba093
MohanRaj <mraj86698@gmail.com>
2022-06-23 11:18:02 +0530

    Welcome Message

---
+++ src/lineComp/LineComparison.java
@@ -0,0 +1,10 @@
+package lineComp;
+
+public class LineComparison {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Line Comparison Computation program on Master Branch");
+
+	}
+
+}
